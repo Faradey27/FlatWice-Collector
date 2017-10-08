bd26db16abf62599aa9f7292223d7823e02b480b
Faradey27 <andrii.tiertyshnyi@gmail.com>
2017-10-09 02:33:34 +0300

    Refactoring:  - added babel  - refactored code to classes  - moved to async await where possible  - cleaned code

---
+++ src/Resources/Olx/Olx.js
@@ -0,0 +1,26 @@
+const cheerio = require('cheerio');
+import OlxFlatDetailsPage from './OlxFlatDetailsPage'
+
+class Olx {
+  static defaultUrl = 'https://www.olx.ua/nedvizhimost/arenda-kvartir/kiev/';
+
+  constructor(body, url = Olx.defaultUrl) {
+    this.$ = cheerio.load(body);
+    this.url = url;
+  }
+
+  get title(){
+    return this.$('.offer-titlebox h1').text();
+  }
+
+  get listOfLinksToFlats() {
+    return this.$('.wrap .offer h3 .linkWithHash').map((index, element) => this.$(element).attr('href')).get();
+  }
+
+  scrapeInfoAboutFlat = (url) => {
+    const flat = new OlxFlatDetailsPage(this.$, url);
+    return flat.toJson();
+  }
+}
+
+export default Olx;
